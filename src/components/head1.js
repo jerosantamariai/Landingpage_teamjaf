007ed78694e086500d3a23d0f53a0655515686ef
Flkis  Flkis <flkisflkis@MacBook-Pro-de-Flkis.local>
2020-03-10 16:21:20 -0300

    corrige className

--- src/components/head1.js
+++ src/components/head1.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Head1 = (props) => {
     return (
-        <div class="container">
-            <div class="row">
-                <div class="col-8 p-5">
+        <div className="container">
+            <div className="row">
+                <div className="col-8 p-5">
                     <h2>The irst Heading <span style="color: grey;">Will Catch Your Eye</span></h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum error ut magnam temporibus.
                     Asperiores dignissimos repellendus error voluptas, obcaecati molestiae suscipit modi illo sed
                     aspernatur. Labore eius nulla sequi harum.</p>
                 </div>
-                <div class="col-4 p-5">
+                <div className="col-4 p-5">
                     <img src="../public/assets/img/1.jpg" alt="Breaking Bad Image" style="border-radius: 50%;width: 100%;" />
                 </div>
             </div>
--- src/components/head2.js
+++ src/components/head2.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Head2 = (props) => {
     return (
-        <div class="container" id="second_heading">
-            <div class="row">
-                <div class="col-8 p-5">
+        <div className="container" id="second_heading">
+            <div className="row">
+                <div className="col-8 p-5">
                     <img src="../public/assets/img/2.jpg" style="border-radius: 50%;" alt="..."/>
             </div>
-                    <div class="col-4 p-5">
-                        <h4>The Second Heading <span class="text-muted">Is Pretty Cool Too
+                    <div className="col-4 p-5">
+                        <h4>The Second Heading <span className="text-muted">Is Pretty Cool Too
                     </span></h4>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                         Voluptates dolor minus sed temporibus quidem itaque beatae expedita vel fuga quam
